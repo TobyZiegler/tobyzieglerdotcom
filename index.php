39dcdced07301359ed97878231c359a71ad1d5a1
TJ Ziegler <tziegle@C09336.local>
2018-03-15 18:29:50 -0500

    add further improvements to linking mechanics

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
-    $homefolder = "/";
-    $server = $_SERVER['SERVER_NAME'];
-    if ($server == "localhost") $homefolder = ":8888/tobyzieglerdotcom/";
-    $path = $server . $homefolder;
-
-    $keywords = ", index";
+    $keywords = ", index, home";
     $description = "The home page for Toby Zieger's personal site.";
     $title = "Home Page";
     $header = "<h2>Hello, and welcome Toby Ziegler's site.</h2>";
-    $content = ($path . 'pages/homecontent.php');
-    include($path . 'pages/templates/baseview.php');
+    $content = ('pages/homecontent.php');
+    include('pages/templates/baseview.php');
 ?>
-<p>content = <?php echo $content?></p>
-<p>baseview = <?php echo ($path . 'pages/templates/baseview.php')?></p>
--- pages/templates/baseview.php
+++ pages/templates/baseview.php
@@ -9,7 +9,13 @@
     <title>
         <?php echo $title; ?>
     </title>
-    <?php if ($path == "") $path = "localhost:8888/tobyzieglerdotcom/";?>
+
+    <?php
+    $homefolder = "/";
+    $server = $_SERVER['SERVER_NAME'];
+    if ($server == "localhost") $homefolder = ":8888/tobyzieglerdotcom/";
+    $path = $server . $homefolder;
+    ?>
 
     <?php include("favicon.php");?>
         <link rel="stylesheet" href="<?php echo ($path . 'css/reset.css')?>">
@@ -36,9 +42,6 @@
             <?php include("footer.php");?>
         </footer>
         </div>
-
-            <p>path = <?php echo $path;?>
-
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
