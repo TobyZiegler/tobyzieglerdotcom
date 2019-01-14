aac76d363fb99ddb7af1941b0836c99f92cdc47c
Toby Ziegler - MacBook <MacZiegler@gmail.com>
2019-01-14 01:44:10 -0600

    reorganize approach to w3 format with angularJS partials

--- js/navApp.module.js
+++ js/navApp.module.js
@@ -43,6 +43,8 @@ app.config(function ($routeProvider) {
 app.controller('pageController', function ($scope) {
     $scope.message = "Welcome to Toby's pages";
     $scope.sayHello = 'pageController says, "Hello World."'
+    $scope.shortName = "Toby's Pages"
+    $scope.plusName = "Welcome to "
     console.log("Page Controller reporting for duty.");
 });
 
--- js/tobyscripts.js
+++ js/tobyscripts.js
@@ -1,3 +1,47 @@
+/*
+Modal Image Gallery code courtesy w3schools
+https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_parallax&stacked=h
+*/
+function onClick(element) {
+        document.getElementById("img01").src = element.src;
+        document.getElementById("modal01").style.display = "block";
+        var captionText = document.getElementById("caption");
+        captionText.innerHTML = element.alt;
+      }
+  
+/*
+Used to toggle the menu on small screens when clicking on the menu button
+Toggle code courtesy w3schools
+https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_parallax&stacked=h
+*/
+function toggleFunction() {
+        var x = document.getElementById("navDemo");
+        if (x.className.indexOf("w3-show") == -1) {
+          x.className += " w3-show";
+        } else {
+          x.className = x.className.replace(" w3-show", "");
+        }
+      }
+  
+/*
+Change style of navbar on scroll
+Navbar scroll code courtesy w3schools
+https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_parallax&stacked=h
+*/
+window.onscroll = function () { myFunction() };
+      function myFunction() {
+        var navbar = document.getElementById("myNavbar");
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+          navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        } else {
+          navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        }
+      }
+  
+/*
+reveal code courtesy Banana from stackoverflow
+https: //stackoverflow.com/users/3406576/banana
+*/
 function reveal(id) {
     var e = document.getElementById(id);
     if (e.style.display == 'inline') {
@@ -10,7 +54,3 @@ function reveal(id) {
         e.style.display = 'inline';
     }
 }
-/*
-reveal code courtesy Banana from stackoverflow
-https: //stackoverflow.com/users/3406576/banana
-*/
