fff3d92b59a8200265b4f78fa21e15b0caf2f06f
Toby Ziegler - MacBook <Toby@TobyZiegler.com>
2019-03-28 23:37:30 -0500

    remove php and old site, reorganize assets

--- pages/notes/notesindex.php
+++
@@ -1,8 +0,0 @@
-<?php
-$keywords = ", index, notes";
-$description = "Notes and thoughts shared for Toby Zieger's personal site.";
-$title = "Notes Index";
-$header = "<h1>Notes and Thoughts</h1>";
-$content = 'notescontent.php';
-include '../templates/baseview.php';
-?>
